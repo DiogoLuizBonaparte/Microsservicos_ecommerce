391245a24cab30291337df0a209d86fc3a40f159
Bonaparte <diogo322@hotmail.com>
2024-01-22 23:36:30 -0300

    First

---
+++ LojaOnline.ProductAPI/Model/Context/SQLContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LojaOnline.ProductAPI.Model.Context
+{
+    public class SQLContext : DbContext
+    {
+        public SQLContext() { }
+
+        public SQLContext(DbContextOptions<SQLContext> options) : base(options) { }
+
+        public DbSet<Product> products { get; set; }    
+
+    }
+}
